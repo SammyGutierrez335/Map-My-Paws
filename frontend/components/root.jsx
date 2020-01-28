14bcf34737949f53469f7dec712af3d4cf41f04b
Sam <sammygutierrez335@gmail.com>
2020-01-28 17:17:38 -0600

    add app and root component and refactor index.jsx

---
+++ frontend/components/root.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Provider } from "react-redux"
+import { HashRouter } from "react-router-dom"
+import App from "./app";
+
+//accepts store as a prop
+const Root = ({ store }) => (
+    <Provider store={store}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </Provider>
+)
+//App will be rendered for all routes in your app. 
+//Routes are defined within App.jsx
+
+export default Root
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from "./components/root"
+import configureStore from './store/store';
+
 // import * as apiUtil from "./util/session_api_util.js"
 // used for testing
-// import Root from "./components/root"
 
-import configureStore from "./store/store"
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root")
     const store = configureStore();
+    // TESTING START
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    ReactDOM.render(<h1>Welcome to Map my Paws</h1>, root)
-    // ReactDOM.render(<Root store={store} />, root)
+    // TESTING END
+
+    ReactDOM.render(<Root store={store} />, root)
 })
+//renders root component passing in store as a prop.
+
 
 //used for testing
 // window.login = apiUtil.login
