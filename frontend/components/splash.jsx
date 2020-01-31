dcfcc2fa4c317b56491d0e43e78d47dacfe8971d
Sam <sammygutierrez335@gmail.com>
2020-01-31 00:25:44 -0600

    resize main size and move splash text to footer

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -6,13 +6,9 @@ import React from 'react'
 const splash = () => {
     return (
         <div>
-            <span>
-                <h4> At Map My Paws, we believe exercise is the key to a happy and health dog.</h4>
-            </span>
-            <br></br>
-            <span>
-                <h4> Map My Paws is the innovative app that allows you to track your pooch's pawdometer progress!</h4>
-            </span>
+            <footer>
+
+            </footer>
         </div>
     )
 }
