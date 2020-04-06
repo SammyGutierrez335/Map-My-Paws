2502aac6dd2d1eef04fa769ffc6f498651d40867
Sammy Gutierrez <sammygutierrez335@gmail.com>
2020-04-06 15:20:04 -0500

    updates homepage

--- frontend/components/home_page/homepage.jsx
+++ frontend/components/home_page/homepage.jsx
@@ -2,33 +2,16 @@
 
 import { render } from "react-dom"
 import React from 'react'
-import Navlinks from '../header/navlinks.jsx'
-import GreetingContainer from "../greeting/greeting_container"
+
 
 class Homepage extends React.Component {
 
 
-    render() {
-        return (
+    render() {(
             <div className="home_page_window" >
-                <header>
-                    <Link to="/my_home/#user_dashboard">
-                        <img src={window.logo} />
-                    </Link>
-                    <Navlinks />
-                    <GreetingContainer />
-                </header>
-                <Switch>
-                    <ProtectedRoute exact path="/my_home/#user_dashboard" component={homepage} />
-                    <Route exact path="/walks" component={WalksHome} />
-                    <ProtectedRoute exact path="/walks/create" component={NewWalk} />
-                </Switch>
                 <div id="previous_walk_container">Previous Walks</div>
             </div>
-        )
-    }
-
-
+        )}
 }
 
 export default Homepage
