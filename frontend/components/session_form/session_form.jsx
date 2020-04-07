9d35cb02117226b50a591ce1378eb3c4072d0a89
Sammy Gutierrez <sammygutierrez335@gmail.com>
2020-04-06 23:56:33 -0500

    corrects login url

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -23,7 +23,7 @@ class SessionForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const user = Object.assign({}, this.state);
-    this.props.processForm(user).then(() => { this.props.history.push("/my_home/#user_dashboard") })
+    this.props.processForm(user).then(() => { this.props.history.push("/user_dashboard") })
   }
 
   demoUserLogin(e) {
