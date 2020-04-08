7b3b68dfa59d34bb7b9012b318fec7d16d2859a8
Sammy Gutierrez <sammygutierrez335@gmail.com>
2020-04-08 15:11:19 -0500

    adds post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:index, :create]
+    resource :user, only:[:create]
     resource :session, only: [:create, :destroy, :show]
     resources :walks, only: [:index, :create, :show]
     resources :waypoints, only: [:create, :index]
